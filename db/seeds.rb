36fb461d1fe5185b9289286b0b6df504fb117fee
benjamin maureau <benjamin.maureau@hotmail.fr>
2021-06-09 14:52:52 +0200

    restaurants with no style

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  category = ["chinese", "italian", "japanese", "french", "belgian"].sample
+  new_restaurant = Restaurant.new(name: Faker::Name.name, address: Faker::Address.full_address, category: category)
+  new_restaurant.save
+end
